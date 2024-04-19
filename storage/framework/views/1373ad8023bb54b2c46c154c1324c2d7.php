8e67f69aea160eb6cccd34bac39518fb09cdc968
benzomai09 <brent.boombox@gmail.com>
2024-04-19 12:56:08 +0800

    added favicon

--- storage/framework/views/1373ad8023bb54b2c46c154c1324c2d7.php
+++ storage/framework/views/1373ad8023bb54b2c46c154c1324c2d7.php
@@ -5,15 +5,15 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Dashboard - NiceAdmin Bootstrap Template</title>
+  <title>Social Solutions</title>
   <!-- CSRF Token -->
   <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="../assets/img/favicon.png" rel="icon">
-  <link href="../assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="../assets/img/socsal-favicon.webp" rel="icon">
+  <link href="../assets/img/socsal-favicon.webp" rel="apple-touch-icon">
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
